eab7ef3bdae70311bd7ce6ff29f074858be9d02f
Anindo <anindo.ah@gmail.com>
2024-02-15 20:20:08 +0600

    total price display update

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 // Get each card rom HTML nad show them to cart.
 let itemCount = 1;
+let totalPrice = 0;
 const cards = document.getElementsByClassName("card");
 for (let card of cards) {
   card.addEventListener("click", function () {
-    const h3 = card.querySelector("h3");
-    const h3Text = h3.innerText;
-    console.log(h3Text);
+    const h3ItemTitle = card.querySelector("h3");
+    const itemName = h3ItemTitle.innerText;
 
+    // get item name list
     const titleContainer = document.getElementById("title-container");
     const p = document.createElement("p");
-    p.innerText = itemCount+ '. ' + h3Text;
+    p.innerText = itemCount+ '. ' + itemName;
     titleContainer.appendChild(p);
     itemCount ++;
 
+    // get total price
+    const priceElement = card.querySelector('span');
+    const price = parseFloat(priceElement.innerText.split(' ')[1]);
+    totalPrice = totalPrice + price;
+    const totalPriceElement = document.getElementById('totalPrice');
+    totalPriceElement.innerText = totalPrice;
     
   });
+  
 }
 
